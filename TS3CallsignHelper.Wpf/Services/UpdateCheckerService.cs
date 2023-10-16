47e6101ed7462f4c148fd471bb68505d445812d7
RagingLightning <RagingLightningCode@gmail.com>
2023-10-16 13:34:26 +0200

    Version 2.1.0

--- TS3CallsignHelper.Wpf/Services/UpdateCheckerService.cs
+++ TS3CallsignHelper.Wpf/Services/UpdateCheckerService.cs
@@ -3,14 +3,13 @@ using System.Net;
 
 namespace TS3CallsignHelper.Wpf.Services;
 internal class UpdateCheckerService {
-  public static string VERSION = "2.0.1";
+  public static string VERSION = "2.1.0";
 
   public static string? HasUpdate() {
     WebRequest UpdateRequest = WebRequest.Create("https://raw.githubusercontent.com/RagingLightning/TS3CallsignHelper/deploy/version.dat");
     string UpdateResponse = new StreamReader(UpdateRequest.GetResponse().GetResponseStream()).ReadToEnd();
     string[] NewVersion = UpdateResponse.Split('.');
     string[] CurrentVersion = VERSION.Split('.');
-    bool update = false;
     for (int i = 0; i < NewVersion.Length; i++) {
       if (int.Parse(NewVersion[i]) > int.Parse(CurrentVersion[i])) {
         return UpdateResponse;
